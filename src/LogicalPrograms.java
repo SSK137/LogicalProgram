ed13409b99de4373418be60f7356eeedef15e4d7
SSK137 <saurabhkodam@gmail.com>
2022-05-02 12:18:03 +0530

    Logical Program : Perfect Number

--- src/LogicalPrograms.java
+++ src/LogicalPrograms.java
@@ -1,23 +1,21 @@
-public class LogicalPrograms {
+import java.util.*;
+public class LogicalPrograms{
     public static void main(String[] args) {
-        System.out.println("Welcome To Logical Programming");
-        // veriable declaration
-        int FirstNumber=0;
-        int SecondNumber=1;
-        int sum=0;
-        /*In Every Fibonacci series it start with 0 and 1 so
-        we pritn them first directly
-         */
-        System.out.println(FirstNumber);
-        System.out.println(SecondNumber);
-        //Calculating Fibonacci Series
-        for(int i=FirstNumber;i<=5;i++){
-            for(int j=SecondNumber;j<=5;j++){
-                sum=FirstNumber+SecondNumber;
-                FirstNumber=SecondNumber;
-                SecondNumber=sum;
-                System.out.println(sum);
+        int n, sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any integer you want to check:");
+        n = s.nextInt();
+        for(int i = 1; i < n; i++)
+        {
+            if(n % i == 0) {
+                sum = sum + i;
             }
+        }
+        if(sum == n) {
+            System.out.println("Given number is Perfect");
+        }
+        else {
+            System.out.println("Given number is not Perfect");
         }
     }
 }
